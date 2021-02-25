c380225da19c31adec03b210ebad1e991c8e892f
Gaurav Ghongde <7gauravghongde@gmail.com>
2021-02-26 00:04:37 +0530

    css fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProjectViewComponent } from './projects/project-view/project-view.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: 'projects',
@@ -24,6 +39,10 @@ const routes: Routes = [
         // data: { editorMode: EditorMode.ADD }
       },
     ]
+  },
+  {
+    path: '**',
+    component: LandingPageComponent
   },
 ];
 
